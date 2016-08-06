379bc0f2778e9b17b9df97e22d5eea16ea4d881e
shdhumale@gmail.com <shdhumale@gmail.com>
2016-08-06 12:53:16 +0530

    Simple Fragment example

---
+++ app/src/main/java/siddhu/test/com/myfregment/ListOfArticlesFragment.java
@@ -0,0 +1,100 @@
+package siddhu.test.com.myfregment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import siddhu.test.com.myfregment.network.NewsAPI;
+import siddhu.test.com.myfregment.objects.Article;
+import siddhu.test.com.myfregment.objects.CommonUsage;
+import siddhu.test.com.myfregment.objects.NewsApiArticleResponse;
+import siddhu.test.com.myfregment.objects.Source;
+
+
+/**
+ * Created by admin on 8/6/2016.
+ * List of Articles--ViewPager--Fragment--Recycle--Multiple Elements
+ */
+
+public class ListOfArticlesFragment extends Fragment {
+
+    private static final String KEY_POSITION = "position";
+    private static final int DEFAULT_POSITION = 0;
+
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+
+    Source mySource;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int position = getArguments().getInt(KEY_POSITION,DEFAULT_POSITION);
+        mySource = CommonUsage.getAllNewSource().get(position);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(container.getContext()).inflate(R.layout.activity_main,container,false);
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.activity_main_newsitems);
+        progressBar = (ProgressBar) view.findViewById(R.id.activity_main_progress);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        Call<NewsApiArticleResponse> responseCall = NewsAPI.getNewsAPI().getArticles(mySource.getId(), mySource.getSortBysAvailable().get(0));
+        responseCall.enqueue(new Callback<NewsApiArticleResponse>() {
+            @Override
+            public void onResponse(Call<NewsApiArticleResponse> call, Response<NewsApiArticleResponse> response) {
+                progressBar.setVisibility(View.GONE);
+                NewsApiArticleResponse newsApiArticleResponse = response.body();
+                List<Article> articles = newsApiArticleResponse.getArticles();
+
+
+                CommonUsage.setAllNewArticle(articles);
+
+                NewAdapter newAdapter = new NewAdapter(CommonUsage.getAllNewArticle());
+                recyclerView.setAdapter(newAdapter);
+
+                Toast.makeText(getActivity(), "Fragment article response received", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<NewsApiArticleResponse> call, Throwable t) {
+                Toast.makeText(getActivity(), "Fragement article error received", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
+
+    }
+
+    public static ListOfArticlesFragment generateFragment(int position)
+    {
+        ListOfArticlesFragment listOfArticlesFragment = new ListOfArticlesFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_POSITION, position);
+        listOfArticlesFragment.setArguments(bundle);
+        return listOfArticlesFragment;
+    }
+}
---
+++ app/src/main/java/siddhu/test/com/myfregment/SwipeHomeActivity.java
@@ -0,0 +1,76 @@
+package siddhu.test.com.myfregment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import siddhu.test.com.myfregment.network.NewsAPI;
+import siddhu.test.com.myfregment.objects.CommonUsage;
+import siddhu.test.com.myfregment.objects.NewsApiSourcesResponse;
+import siddhu.test.com.myfregment.objects.Source;
+
+/**
+ * Created by admin on 8/6/2016.
+ */
+
+public class SwipeHomeActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_home);
+        viewPager = (ViewPager) findViewById(R.id.activity_swipe_view_pager);
+        Call<NewsApiSourcesResponse> responseCall = NewsAPI.getNewsAPI().getSources();
+        responseCall.enqueue(new Callback<NewsApiSourcesResponse>() {
+            @Override
+            public void onResponse(Call<NewsApiSourcesResponse> call, Response<NewsApiSourcesResponse> response) {
+                List<Source> allSource = response.body().getSources();
+                CommonUsage.setAllNewSource(allSource);
+                ViewPagerAdapter viewPageAdapter = new ViewPagerAdapter(getSupportFragmentManager(),allSource);
+                viewPager.setAdapter(viewPageAdapter);
+                //Toast.makeText(SwipeHomeActivity.this, "Success Call", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<NewsApiSourcesResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public class ViewPagerAdapter extends FragmentStatePagerAdapter
+    {
+        List<Source> sources;
+
+        public ViewPagerAdapter(FragmentManager fm, List<Source> sources) {
+            super(fm);
+            this.sources=sources;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return ListOfArticlesFragment.generateFragment(position);
+        }
+
+        @Override
+        public int getCount() {
+            return sources.size();
+        }
+    }
+
+
+}
